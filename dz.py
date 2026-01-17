f37fbea70946f30b0b05994022b771f20f47857d
Iskord <serhiytatsyk@gmail.com>
2026-01-17 13:21:44 +0100

    Fix indentation and formatting in dz.py

--- dz.py
+++ dz.py
@@ -108,18 +108,14 @@ class Shape(ABC):
     @abstractmethod
     def area(self):
         pass
-
 class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
-
     def area(self):
         return 3.14 * self.radius ** 2
-    
     class Square(Shape):
     def __init__(self, side):
         self.side = side
-
     def area(self):
         return self.side ** 2
 
